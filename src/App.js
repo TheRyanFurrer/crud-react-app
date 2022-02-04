0195d31418aa5a449009798bce0382f9f9c79813
Ryan Furrer <rfurrertech@gmail.com>
2022-02-03 21:03:41 -0500

    Began building out project with Tailwind CSS components and placeoholder content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
+import AddUserField from './components/AddUserField'
+import ViewUserField from './components/ViewUserField'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container max-w-7xl mx-auto">
+      <Header />
+      <div id='content-container' className=" mx-auto grid grid-cols-2 gap-8 p-8">
+        <AddUserField />
+        <ViewUserField />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/ViewUserField.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ViewUserField = () => {
+  return (
+    <div className="grid grid-cols-1 grid-flow-row gap-2">
+      <h2 className="text-3xl">View Users</h2>
+      <form action="" className="grid grid-cols-1 grid-flow-row gap-y-4">
+      </form>
+    </div>
+  )
+}
+
+export default ViewUserField
